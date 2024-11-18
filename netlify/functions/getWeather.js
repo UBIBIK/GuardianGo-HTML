2d2ab7e36c19c7964e450e32a6c66aa736159cdb
최우빈 <3jdnqls@naver.com>
2024-11-18 16:31:19 +0900

    날씨 정보 표시 https사용

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -1,29 +1,23 @@
-const https = require('https');
+const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
     const { nx, ny, baseDate, baseTime, serviceKey } = event.queryStringParameters;
 
     const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${serviceKey}&numOfRows=10&pageNo=1&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
 
-    return new Promise((resolve, reject) => {
-        https.get(url, (response) => {
-            let data = '';
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-            response.on('end', () => {
-                resolve({
-                    statusCode: 200,
-                    headers: { 'Content-Type': 'application/json' },
-                    body: data,
-                });
-            });
-        }).on('error', (error) => {
-            reject({
-                statusCode: 500,
-                body: JSON.stringify({ error: error.message }),
-            });
-        });
-    });
+        return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };
